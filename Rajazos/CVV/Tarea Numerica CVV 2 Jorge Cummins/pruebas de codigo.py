4b133bccf25428c399d3439778dccd6ed50bd760
Jorge Cummins <131581057+JorgeCSH@users.noreply.github.com>
2023-05-25 01:16:38 -0400

    Add files via upload

--- Rajazos/CVV/Tarea Numerica CVV 2 Jorge Cummins/pruebas de codigo.py
+++ Rajazos/CVV/Tarea Numerica CVV 2 Jorge Cummins/pruebas de codigo.py
@@ -1,32 +1,55 @@
 # Aca hago pruebas con los codigos
 import numpy as np
 
-def phiExtra(w1,w2,b,x0):
-    phi = [w1, w2, b, x0]
-    return phi
-def Rphi(phi):
-    w1 = float(phi[0])
-    w2 = float(phi[1])
-    b = float(phi[2])
-    x0 = phi[3]
-    x01 = x0[0]
-    x02 =x0[1]
-    f = (w1*x01+w2*x02+b)
-    def sigmoidR(s):
-        e = np.exp(s)
-        denominador = (1+e)
-        sig = e/denominador
-        return sig
-    ev = sigmoidR(f)
-    return ev
 
-def costo(phi):
-    rphi = Rphi(phi)
+# Funcions o componentes Globales
 
 
+# phi = np.array([[w1], [w2], [b]])
 
 
+# Toma dos numeros y devuelve la funcion sigmoid o su derivada
+#           -> 0, devuelve la funcion.
+#           -> 1, devuelve la derivada en torno a s
+def sigmoid(s, tipo):
+    e = np.exp(s)
+    denominador = (1+e)
+    if tipo == 0:
+        sigmoide = (e)/(denominador)
+        return sigmoide
+    elif tipo == 1:
+        sigmoide = (e)/((denominador)**2)
+        return sigmoide
 
 
-#def gradienteConjugado():
+# Entrega la red neuronal realizada.
+# phi contiene los pesos y, el vector bias
+# x es el vector input
+# tipo es que se quiere calcular.
+#           -> 0 = valor sin evaluar "x"
+#           -> 1 = valor tras evaluar "sigma(x)"
+# Esto es para obtener R(phi), despues hay que hacer C(phi)    D:
+# dif es si se quiere el valor derivado o no (algunos, no todos xd)
+#           -> 0 = no derivado
+#           -> 1 = derivado
+# CAMBIOS POR HACER:
+#           -> Para evitar definir VARIAS VARIABLES (lo dijo xd), usar
+#              multiplicacion de matrices
+def realizacion(phi, x, tipo, dif):
+    pesos = np.array([[phi[0], phi[1]]])
+    b = phi[2]
+    valoresTeorico = np.dot(np.transpose(pesos), x)
+    valoresNumerico = float(valoresTeorico)
+    operando = valoresNumerico + float(b)
+    if tipo == 0:
+        evalu = operando
+        return evalu
+    elif tipo == 1:
+        evalu = sigmoid(operando, dif)
+        return evalu
 
+
+# Derivadas Parciales
+# derivada del tipo dc/dw1
+
+#def parcialw1():
