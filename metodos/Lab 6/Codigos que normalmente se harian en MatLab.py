eed3cf6b6bd580f4572b44ef464b2da3ccd4050b
jc <jorge.cumminz@gmail.com>
2023-06-06 02:45:27 -0400

    Lab 6

--- metodos/Lab 6/Codigos que normalmente se harian en MatLab.py
+++ metodos/Lab 6/Codigos que normalmente se harian en MatLab.py
@@ -9,6 +9,7 @@ import  matplotlib.pyplot as plt
 # 1[k\Omega] = 1000[\Omega]
 
 
+intento = input('Inserte numero de intento de realizacion (y de ser necesario, alguna info para diferenciar)  ')
 input('Iniciar Parte 1: presiona ENTER ')
 
 # Funcion para interactuar
@@ -35,9 +36,14 @@ print('Ahora los datos medidos ')
 OY = Cotorra(Datos, valorL = [])
 #print(OX)
 #print(OY)
+# Guardar datos
+Almacenes_Paris = open("No es un SSD lamentablemente.txt", "w")
+lin = intento
+Almacenes_Paris.write('\n'+lin + str(OX) + str(OY))
 
+input('RECUERDA GUARDAR LAS MEDICIONES MANUALMENTE DESPUES DE CADA MEDICION ')
 
-grafico1 = input('Mostrar Grafico?, presione: 1 si es asi, presione ENTER si no ')
+grafico1 = input('Mostrar Grafico?, presione: 2 si es asi, presione ENTER si no ')
 if grafico1 == '1':
     plt.figure(figsize=(7,5))
     plt.scatter(OX[0], OY[0], label = "Transferencia medida ")
@@ -58,7 +64,8 @@ if grafico1 == '1':
 
 
 
-input('Iniciar Parte 2: presiona ENTER')
+iintento = input('Inserte numero de intento de realizacion (y de ser necesario, alguna info para diferenciar)  ')
+input('Iniciar Parte 1: presiona ENTER ')
 
 # Funcion para interactuar
 print('A continuacion Pediran las frecuencias ')
@@ -85,9 +92,10 @@ OYy = Cotorra(Datos, valorL = [])
 #print(OX)
 #print(OY)
 
+input('RECUERDA GUARDAR LAS MEDICIONES MANUALMENTE DESPUES DE CADA MEDICION ')
 
-grafico1 = input('Mostrar Grafico?, presione: 1 si es asi, presione ENTER si no ')
-if grafico1 == '1':
+grafico2 = input('Mostrar Grafico?, presione: 2 si es asi, presione ENTER si no ')
+if grafico2 == '2':
     plt.figure(figsize=(7,5))
     plt.scatter(OXx[0], OYy[0], label = "Transferencia medida ")
     plt.loglog(OXx[0], OYy[0], label="Ajuste Logaritmo base 10 ($Log_{10}$)", color = "green")
@@ -107,4 +115,13 @@ if grafico1 == '1':
 
 
 
+# Guardar datos
+line = iintento
+Almacenes_Paris.write('\n'+line + str(OXx) + str(OYy))
+Almacenes_Paris.close()
+print()
+input('RECUERDA GUARDAR LAS MEDICIONES MANUALMENTE DESPUES DE CADA MEDICION ')
+print()
+print('Gracias, vuelve pronto (ojala no, significa que te echaste el ramo) ')
 
+###################################___Lab 6___###################################
