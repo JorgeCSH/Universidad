9cb57147659bc7bf83f77325fe60d5a91f7831b4
jorge <jorge.cummins@ug.uchile.cl>
2024-08-10 23:01:00 -0400

    Tarea 1 bruto deberia estar listo, ahora con problemas solucionados en la tanda de 5....verificar igual despues

--- Archivos por ramo/DCC/Algoritmos/Tareas/Tarea 1.py
+++ Archivos por ramo/DCC/Algoritmos/Tareas/Tarea 1.py
@@ -13,8 +13,8 @@ print("El orden en que se insertan los equipos es el orden en que se realizan lo
 equipo1 = input("Ingrese el nombre del equipo 1:  ") 
 equipo = input("Ingrese el nombre del equipo 2: ")
 """
-equipo1 = "Colo Colo"
-equipo2 = "U de Chile"
+equipo1 = "Munich"
+equipo2 = "Valencia"
 
 
 # Funcion muerte subita
@@ -28,28 +28,43 @@ def muerte_subita(equipo1, equipo2):
     elif resultado_subito2 == "gol":
         print("Gana equipo 2, ", equipo2)
 
-def tanda_de_penales(equipo1, equipo2, trix=5):
+def tanda_de_penales(equipo1, equipo2, tiros_iniciales=5):
     tiro = 0
-    tiros_iniciales = trix-1
-    tiros_restantes = tiros_iniciales-tiro
+    tiros_restantes = tiros_iniciales - tiro
     contador_equipo1 = 0
     contador_equipo2 = 0
     while tiro<5:
+        print("Ronda "+ str(tiro+1))
         tiros_restantes = tiros_iniciales - tiro
         tiro_equipo1 = input("Ingrese resultado del tiro de "+ equipo1+ " ")
-        tiro_equipo2 = input("Ingrese resultado del tiro de "+ equipo2+ " ")
         if tiro_equipo1 == "gol":
             contador_equipo1 += 1
-        if tiro_equipo2 == "gol":
-            contador_equipo2 += 1
         if contador_equipo1 > contador_equipo2+tiros_restantes:
             print("Gana equipo 1, ", equipo1)
+            print("Resultado: "+equipo1+ " "+str(contador_equipo1) + ", "+str(equipo2)+ " "+str(contador_equipo2))
             break
-        elif contador_equipo2 > contador_equipo1+tiros_restantes:
+        if contador_equipo2 >= contador_equipo1+tiros_restantes:
             print("Gana equipo 2, ", equipo2)
+            print("Resultado: " + equipo1 + " " + str(contador_equipo1) + ", " + str(equipo2) + " " + str(contador_equipo2))
             break
-        else:
-            tiro = tiro+1
+        tiro_equipo2 = input("Ingrese resultado del tiro de "+ equipo2+ " ")
+        if tiro_equipo2 == "gol":
+            contador_equipo2 += 1
+        if contador_equipo2 > contador_equipo1+tiros_restantes:
+            print("Gana equipo 2, ", equipo2)
+            print("Resultado: " + equipo1 + " " + str(contador_equipo1) + ", " + str(equipo2) + " " + str(contador_equipo2))
+            break
+        if contador_equipo1 >= contador_equipo2+tiros_restantes:
+            print("Gana equipo 1, ", equipo1)
+            print("Resultado: "+equipo1+ " "+str(contador_equipo1) + ", "+str(equipo2)+ " "+str(contador_equipo2))
+            break
+        print("Resultado: " + equipo1 + " " + str(contador_equipo1) + ", " + str(equipo2) + " " + str(contador_equipo2))
+        print("fin ronda "+str(tiro+1))
+        print()
+        print(contador_equipo1, contador_equipo2, tiros_restantes)
+        tiro = tiro+1
+
+"""        
     if tiros_restantes == 1:
         tiro_equipo1 = input("Ingrese resultado del tiro de " + equipo1 + " ")
         tiro_equipo2 = input("Ingrese resultado del tiro de " + equipo2 + " ")
@@ -64,13 +79,10 @@ def tanda_de_penales(equipo1, equipo2, trix=5):
         else:
             muerte_subita(equipo1, equipo2)
 
+"""
+
 
 
 
 
 tanda_de_penales(equipo1, equipo2)
-
-
-
-
-
