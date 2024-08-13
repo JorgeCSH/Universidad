46065f8fda7a258d7487e7d25bcbe37db292879f
jorge <jorge.cummins@ug.uchile.cl>
2024-08-12 22:55:22 -0400

    Molestando un rato con la tarea 1

--- Archivos por ramo/DCC/Algoritmos/Tareas/Tarea 1.py
+++ Archivos por ramo/DCC/Algoritmos/Tareas/Tarea 1.py
@@ -2,18 +2,14 @@
 Corresponde al desarrollo fuera del notebook de la tarea 1, esto para ir probando codigos
 y no tener que estar copiando y pegando en el notebook.
 """
-# print(f"Resultado: {equipo1} {contador_equipo1}, {equipo2} {contador_equipo2}")
 
-
-# Importar librearias
-#import numpy as np
-
-
+# TAREA ORIGINAL, NO BORRAR
 # Aca se ingresan los datos de los equipos que se usaran
 print("El orden en que se insertan los equipos es el orden en que se realizan los tiros de penales.")
-equipo1 = input("Ingrese el nombre del equipo 1:  ") 
+equipo1 = input("Ingrese el nombre del equipo 1: ")
 equipo2 = input("Ingrese el nombre del equipo 2: ")
 print()
+
 
 
 # Funciones
@@ -49,6 +45,7 @@ def muerte_subita(equipo1, equipo2, tiro, contador_equipo1, contador_equipo2):
         print(f"El ganador es: {equipo2}")
 
 
+
 """Funcion Tanda de penales
 
 El proposito de esta funcion es realizar el sistema solicitado para la tanda de penales.
@@ -79,6 +76,8 @@ def tanda_de_penales(equipo1, equipo2, tiros_iniciales=5):
             ganador += 1
             break
         if contador_equipo2 >= contador_equipo1+tiros_restantes:
+            print("El ganador se decide antes de completar los 5 tiros debido a una ventaja insuperable. ")
+            print("Resultado final: ")
             print(f"{equipo1}: {contador_equipo1}")
             print(f"{equipo2}: {contador_equipo2}")
             print(f"El ganador es: {equipo2}")
@@ -88,15 +87,103 @@ def tanda_de_penales(equipo1, equipo2, tiros_iniciales=5):
         if tiro_equipo2 == "gol":
             contador_equipo2 += 1
         if contador_equipo2 >= contador_equipo1+tiros_restantes:
+            print("El ganador se decide antes de completar los 5 tiros debido a una ventaja insuperable. ")
+            print("Resultado final: ")
             print(f"{equipo1}: {contador_equipo1}")
             print(f"{equipo2}: {contador_equipo2}")
             print(f"El ganador es: {equipo2}")
             ganador += 1
             break
         if contador_equipo1 >= contador_equipo2+tiros_restantes:
+            print("El ganador se decide antes de completar los 5 tiros debido a una ventaja insuperable. ")
+            print("Resultado final: ")
             print(f"{equipo1}: {contador_equipo1}")
             print(f"{equipo2}: {contador_equipo2}")
             print(f"El ganador es: {equipo1}")
+            ganador += 1
+            break
+        tiro = tiro+1
+    if ganador == 0:
+        print()
+        print("Resultado despues de los 5 tiros: ")
+        print(f"{equipo1}: {contador_equipo1}")
+        print(f"{equipo2}: {contador_equipo2}")
+        print("Empate! Vamos a muerte subita")
+        print()
+        muerte_subita(equipo1, equipo2, tiro, contador_equipo1, contador_equipo2)
+
+
+
+# Iniciamos el programa
+tanda_de_penales(equipo1, equipo2)
+
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+"""
+print("El orden en que se insertan los equipos es el orden en que se realizan los tiros de penales.")
+equipo1 = input("Ingrese el nombre del equipo 1: ")
+equipo2 = input("Ingrese el nombre del equipo 2: ")
+print()
+
+
+def muerte_subita(equipo1, equipo2, tiro, contador_equipo1, contador_equipo2):
+    resultado_subito1 = input(f"¿Ingrese resultado de el tiro {tiro+1} del jugador del equipo {equipo1}?: ")
+    resultado_subito2 = input(f"¿Ingrese resultado de el tiro {tiro+1} del jugador del equipo {equipo2}?: ")
+    equipo_ganador = ""
+    while resultado_subito1 == resultado_subito2:
+        if resultado_subito1 == "gol" or resultado_subito2 == "gol":
+            muerte_subita(equipo1, equipo2, tiro+1, contador_equipo1+1, contador_equipo2+1)
+        muerte_subita(equipo1, equipo2, tiro + 1, contador_equipo1, contador_equipo2)
+    print(f"Resultado final:")
+    if resultado_subito1 == "gol":
+        contador_equipo1 += 1
+        equipo_ganador = equipo1
+    if resultado_subito2 == "gol":
+        contador_equipo2 += 1
+        equipo_ganador = equipo2
+    print(f"{equipo1}: {contador_equipo1}")
+    print(f"{equipo2}: {contador_equipo2}")
+    print(f"El ganador es: {equipo_ganador}")
+
+
+def mostrar_ganador(equipo1, equipo2, contador_equipo1, contador_equipo2, ganador_insuperable):
+    print("El ganador se decide antes de completar los 5 tiros debido a una ventaja insuperable. ")
+    print("Resultado final: ")
+    print(f"{equipo1}: {contador_equipo1}")
+    print(f"{equipo2}: {contador_equipo2}")
+    print(f"El ganador es: {ganador_insuperable}")
+
+
+def tanda_de_penales(equipo1, equipo2, tiros_iniciales=5):
+    tiro = 0
+    contador_equipo1 = 0
+    contador_equipo2 = 0
+    ganador = 0
+    print(f"{equipo1} comienza primero")
+    while tiro<5:
+        tiros_restantes = tiros_iniciales - tiro
+        tiro_equipo1 = input(f"¿Ingrese resultado de el tiro {tiro+1} del jugador del equipo {equipo1}?: ")
+        if tiro_equipo1 == "gol":
+            contador_equipo1 += 1
+        if contador_equipo1 > contador_equipo2+tiros_restantes:
+            mostrar_ganador(equipo1, equipo2, contador_equipo1, contador_equipo2, equipo1)
+            ganador += 1
+            break
+        if contador_equipo2 >= contador_equipo1+tiros_restantes:
+            mostrar_ganador(equipo1, equipo2, contador_equipo1, contador_equipo2, equipo2)
+            ganador += 1
+            break
+        tiro_equipo2 = input(f"¿Ingrese resultado de el tiro {tiro+1} del jugador del equipo {equipo2}?: ")
+        if tiro_equipo2 == "gol":
+            contador_equipo2 += 1
+        if contador_equipo2 >= contador_equipo1+tiros_restantes:
+            mostrar_ganador(equipo1, equipo2, contador_equipo1, contador_equipo2, equipo2)
+            ganador += 1
+            break
+        if contador_equipo1 >= contador_equipo2+tiros_restantes:
+            mostrar_ganador(equipo1, equipo2, contador_equipo1, contador_equipo2, equipo1)
             ganador += 1
             break
         #print(contador_equipo1, contador_equipo2, tiros_restantes)
@@ -111,5 +198,17 @@ def tanda_de_penales(equipo1, equipo2, tiros_iniciales=5):
         muerte_subita(equipo1, equipo2, tiro, contador_equipo1, contador_equipo2)
 
 
-# Iniciamos el programa
 tanda_de_penales(equipo1, equipo2)
+
+
+"""
+
+
+
+
+
+
+
+
+
+
