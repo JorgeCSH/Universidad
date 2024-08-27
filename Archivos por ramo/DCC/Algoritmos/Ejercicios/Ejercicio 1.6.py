2e752a7b229f5d3a3660d687846936261494a90b
jorge <jorge.cummins@ug.uchile.cl>
2024-08-26 23:13:14 -0400

    Ejercicio 1.6 terminado, no el mismo formato que entregue

--- Archivos por ramo/DCC/Algoritmos/Ejercicios/Ejercicio 1.6.py
+++ Archivos por ramo/DCC/Algoritmos/Ejercicios/Ejercicio 1.6.py
@@ -15,35 +15,39 @@ no corresponde a la variable final, se realiza un ciclo iterativo "for" donde se
 de manera recursiva aumentando en 1 el valor del índice final, además de invertir los valores en la posición "i" con
 el de la posición "ini" para crear una nueva variable de la permutación.
 
-El caso general toma un arreglo x, un valor inicial "ini = 0" y final "fin = len(x) - 1", pues asi recorre todos los 
+El caso general toma un arreglo x, un valor inicial "ini = 0" y final "fin = len(x)", pues asi recorre todos los 
 valores del arreglo.
 '''
 def permutaciones(x, ini, fin):
     if ini == fin:
         print(x)
     else:
-        for i in range(ini, fin + 1):
+        for i in range(ini, fin):
             x[ini], x[i] = x[i], x[ini]
             permutaciones(x, ini + 1, fin)
             x[ini], x[i] = x[i], x[ini]
 
 
+
+
 '''
-Ejemplo de uso. Este originalmente fue probado con arreglo para valores aleatorios, debería ser el mismo resultado pero
+Ejemplo de uso. 
+
+Este originalmente fue probado con arreglo para valores aleatorios, debería ser el mismo resultado pero
 con los números aleatorios. 
-Se decidió dejar el caso más simple para que sea más fácil de visualizar (por lo menos para mi jeje).
+Se decidió dejar el caso más simple para que sea más fácil de visualizar (por lo menos para mi mientras desarrollo
+el ejercicio).
 
-Nota de autor: en el x1 se cambió el 1 para evitar monotonía (y dar una referencia escondida).
+Nota de autor: en el x1 se cambió el 1 para evitar monotonía (y dejar una referencia escondida).
 '''
-
 x1 = np.array([42])                         # Arreglo de 1 elemento, debe retornar una permutacion
 x2 = np.array([1, 2, 3])                    # Arreglo de 3 elementos, debe retornar 6 permutaciones
 x3 = np.array([1, 2, 3, 4, 5])              # Arreglo de 5 elementos, debe retornar 120 permutaciones
-print(x1)                                   # -|    |Imprime las listas usadas. Aca no tiene mucha utilidad, sin embargo
-print(x2)                                   #  |--> |es util si usamos valores aleatorios y ver como, como por ejemplo:
-print(x3)                                   # -|    |x3 = np.random.randint(1, 10, 5)
-permutaciones(x1, 0, len(x1) - 1)       # -|
-permutaciones(x2, 0, len(x2) - 1)       #  |---> Llamamos a la función con los arreglos de ejemplo
-permutaciones(x3, 0, len(x3) - 1)       # -|
+#print(x1)                                   # -|    |Imprime las listas usadas. Aca no tiene mucha utilidad, sin embargo
+#print(x2)                                   #  |--> |es util si usamos valores aleatorios y ver como, como por ejemplo:
+#print(x3)                                   # -|    |x3 = np.random.randint(1, 10, 5)
+#permutaciones(x1, 0, len(x1))       # -|
+#permutaciones(x2, 0, len(x2))       #  |---> Llamamos a la función con los arreglos de ejemplo
+permutaciones(x3, 0, len(x3))       # -|
 
 
