ea58a0345b0c57143a9ef20a3529c5fc774477ec
jorge <jorge.cumminz@gmail.com>
2024-09-15 20:49:02 -0300

    MAMAHUEVO QUE ME QUEDO SIN TIEMPO

--- Archivos_por_ramo/DCC/Grafica/Tareas/Tarea_2/Tarea_2_Jorge_Cummins.py
+++ Archivos_por_ramo/DCC/Grafica/Tareas/Tarea_2/Tarea_2_Jorge_Cummins.py
@@ -15,30 +15,31 @@
     de manera general es bien recibida.
 
     A la hora de realizar esta tarea fue de gran relevancia el material entregado por el equipo docente, de esta forma
-    se agradece este material que sirvio de fundamento para el desarrollo de la tarea, donde implementaciones como
-    la clase "Model", "Camara" y la funcion "models_from_file" fueron adaptaciones de las otorgadas por el equipo
+    se agradece este material que sirvio de fundamento para el desarrollo de la tarea, donde se implementaron clases
+    como la clase "Model", "Camara" y la funcion "models_from_file" fueron adaptaciones de las otorgadas por el equipo
     docente. Tambien, si bien se realizo en un documento aparte, se tomo como inspiracion el template otorgado por los
     profesores.
 
     La realizacion de esta tarea conto con la implementacion de objetos externos, todos provenientes de la pagina
-    "sketcfab", comunidad que se encarga del desarrollo y distribucion de, entre diferentes productos, objetos para
-    modelacion (.obj). Se incorporo al final del documento las referencias respectivas a cada uno de los objetos.
+    "sketcfab", comunidad que se desarrolladores y distribuidores de, entre diferentes productos, objetos para
+    modelacion (.obj).
 =======================================================================================================================
 '''
-# Seccion 1: importamos paquetes ######################################################################################
+# Seccion 1: importamos librerias #####################################################################################
 #######################################################################################################################
-# numpys
+# numpys.
 import numpy as np
+from OpenGL.raw.GLU import gluLookAt
 from numpy._core.multiarray import dtype
 
-# Variantes de pyglet
+# Pyglet y sus variantes, no necesariamente se utilizan todas.
 import pyglet
 from pyglet.gl import *
 from pyglet.math import Mat4, Vec3, clamp
 from pyglet.window import Window, key
 from pyglet.graphics.shader import Shader, ShaderProgram
 
-# Resto de imports
+# Misc.
 import grafica.transformations as tr
 import trimesh as tm
 import os
@@ -48,8 +49,11 @@ sys.path.append(os.path.dirname((os.path.dirname(__file__))))
 
 # Seccion 2: configuracion ############################################################################################
 #######################################################################################################################
-# Clase "Controller" para la ventana
-# Es la que fue entregada por el cuerpo docente
+'''
+Clase "Controller" 
+Esta clase se encarga de controlar la ventana, es la encargada de manejar los eventos de la ventana, como el dibujo.
+Es la que fue entregada por el cuerpo docente
+'''
 class Controller(Window):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -66,18 +70,24 @@ window = Controller(WIDTH, HEIGHT, "Tarea 2")
 keys = pyglet.window.key.KeyStateHandler()
 window.push_handlers(keys)
 window.set_exclusive_mouse(True)
+
+
 # Seccion 3: definimos las clases y funciones que se usaran ###########################################################
 #######################################################################################################################
-# Funcion para insertar los valores de los colores en vez de normalizarlos.
-# El objetivo de esta funcion es tomar los valores de RGB originales [0, 255] y normalizarlos a [0, 1].
-# Fue para insertar los colores sin tener la necesidad de normalizarlos en cada instante.
-# Toma los valores asociados a RGB y devuelve una tupla normalizada.
+'''
+Funcion para insertar los valores de los colores en vez de normalizarlos.
+El objetivo de esta funcion es tomar los valores de RGB originales [0, 255] y normalizarlos a [0, 1].
+Fue para insertar los colores sin tener la necesidad de normalizarlos en cada instante.
+Toma los valores asociados a RGB y devuelve una tupla normalizada.
+'''
 def real_rgb(r, g, b):
     return r / 255, g / 255, b / 255
 
-
-# Clase "Ship" para la nave
-# Clase que se encarga de contener todos los atributos de la nave
+'''
+Clase "Ship" 
+Clase que se encarga de contener todos los atributos de la nave.
+Para su creacion se tomo como inspiracion la clase "Camera" de la clase auxiliar 4.
+'''
 class Ship:
     def __init__(self, size, vertices, indices, speed, pipeline) -> None:
         self.color = np.zeros(3, dtype=np.float32)
@@ -89,7 +99,7 @@ class Ship:
         self.pitch = 0
         self.speed = speed
         self.direction = 0
-        self.front = np.array([0, 0, -1], dtype=np.float32)
+        self.front = np.zeros(3, dtype=np.float32)
         self.up = np.array([0, 1, 0], dtype=np.float32)
         self._buffer = pipeline.vertex_list_indexed(size, GL_TRIANGLES, indices)
         self._buffer.position = vertices
@@ -100,43 +110,44 @@ class Ship:
         self.front[1] = np.sin(self.pitch)
         self.front[2] = np.sin(self.yaw) * np.cos(self.pitch)
         self.front /= np.linalg.norm(self.front)
-
         # Moviemiento basado en direccion de la nave
         self.position += self.direction * self.front* self.speed*dt
 
     def model(self):
         # Montamos matriz de transformacion
         translation = Mat4.from_translation(Vec3(*self.position))
-        rotation = Mat4.from_rotation(self.rotation[0], Vec3(1, 0, 0)).rotate(-np.pi/2+self.rotation[1]-self.yaw, Vec3(0, 1, 0)).rotate(self.rotation[2], Vec3(0, 0, 1))
+        rotation = Mat4.from_rotation(self.rotation[0], Vec3(1, 0, 0)).rotate(self.rotation[1], Vec3(0, 1, 0)).rotate(self.rotation[2], Vec3(0, 0, 1))
         scale = Mat4.from_scale(Vec3(*self.scale))
         return translation @ rotation @ scale
-
 
     def draw(self):
         self._buffer.draw(GL_TRIANGLES)
 
 
-
-# Clase camara
-# Clase que se encarga de tener todos los atributos de la camara.
-class Camara:
+'''
+Clase camara
+Clase que se encarga de tener todos los atributos de la camara.
+Calcula la posicion de la camara de tal manera que sigue al objeto que se le entregue
+'''
+class Camara():
+    # Inicializamos los atributos
     def __init__(self, target) -> None:
         self.target = target # objeto que queremos seguir
         self.position = np.zeros(3, dtype=np.float32)
-        self.front = np.array([0, 0, -1], dtype=np.float32)
-        self.up = np.array([0, 1, 0], dtype=np.float32)
+        self.up = np.zeros(3, dtype=np.float32)
+        self.forward = np.zeros(3, dtype=np.float32)
 
+    # Actualizar la camara
     def update(self, dt):
         self.position = self.target.position - self.target.front + self.target.up
+        # Calculamos el vector forward y el vector up
+        self.forward = self.target.position - self.position
+        self.forward /= np.linalg.norm(self.forward)
 
-        # Actualizar la direccion de la camara
-        self.front = self.target.position - self.position
-        self.front /= np.linalg.norm(self.front)
-
-        self.up = np.array([0, 1, 0], dtype=np.float32)
+        self.up = np.cross(np.cross(self.forward, self.target.up), self.forward)
 
     def view(self):
-        return Mat4.look_at(Vec3(*self.position), Vec3(*(self.position + self.front)), Vec3(*self.up))
+        return Mat4.look_at(Vec3(*self.position), Vec3(*(self.position + self.forward)), Vec3(*self.up))
 
 
 '''
@@ -147,7 +158,8 @@ la nave.
 
 Fue hecha en base a la utilizada y creada en la clase por el auxiliar (auxiliar numero 4).
 '''
-class Model:
+class Model():
+    # Inicializamos los atributos
     def __init__(self, size, vertices, indices, pipeline) -> None:
         self.color = np.zeros(3, dtype=np.float32)
         self.position = np.zeros(3, dtype=np.float32)
@@ -156,6 +168,7 @@ class Model:
         self._buffer = pipeline.vertex_list_indexed(size, GL_TRIANGLES, indices)
         self._buffer.position = vertices
 
+    # Montamos la matriz de transformacion
     def model(self):
         translation = Mat4.from_translation(Vec3(*self.position))
         rotation = Mat4.from_rotation(self.rotation[0], Vec3(1, 0, 0)).rotate(self.rotation[1], Vec3(0, 1, 0)).rotate(
@@ -163,6 +176,7 @@ class Model:
         scale = Mat4.from_scale(Vec3(*self.scale))
         return translation @ rotation @ scale
 
+    # Dibujamos el objeto
     def draw(self):
         self._buffer.draw(GL_TRIANGLES)
 
@@ -183,14 +197,15 @@ def models_from_file(path, clase, speed, pipeline):
     else:
         meshes = [geom]
     models = []
-
+    # Dependiendo de la clase, se creara un objeto u otro.
+    # En caso de ser "model", se creara un objeto de la clase "Model"
     if clase == "model":
         for m in meshes:
             m.apply_scale(2.0 / m.scale)
             m.apply_translation([-m.centroid[0], 0, -m.centroid[2]])
             vlist = tm.rendering.mesh_to_vertexlist(m)
             models.append(Model(vlist[0], vlist[4][1], vlist[3], pipeline))
-
+    # En caso de ser "ship", se creara un objeto de la clase "Ship"
     elif clase == "ship":
         for m in meshes:
             m.apply_scale(2.0 / m.scale)
@@ -244,14 +259,18 @@ void main() {
     pipeline = pyglet.graphics.shader.ShaderProgram(vert_program, frag_program)
 
 
-
     # Aca van los objetos que se van a usar en la escena
+    # Cargamos la nave usando un modelo de la comunidad de Sketchfab.
+    Spacecraft = models_from_file("objects/space_shuttle.obj", "ship", 5, pipeline)[0] # Similar al resto de objetos, solo que ahora se usa "Ship" y se le otorga una velocidad diferente de 0.
+    Spacecraft.color = real_rgb(150, 140, 150)
+    Spacecraft.scale = [.5] * 3
+    Spacecraft.position = [2, 1, 0]
+
     # Configuramos el sol.
     sol = models_from_file("objects/sun.obj", "model", 0, pipeline)[0]  # Cargamos el path y creamos usando "Model".
     sol.color = real_rgb(255, 255, 0)                                            # Le damos color usando la funcion "real_rgb" para normalizarla.
     sol.scale = [1.5]*3                                                                   # Escalamos el sol.
     sol.position = [0, 0, 0]                                                              # Posicion, lo situamos en el origen
-
 
     # Configuracion planetas
     # Planeta 1, el mas cercano al sol, usamos el modelo 3D dado por el cuerpo docente.
@@ -279,13 +298,7 @@ void main() {
     planet_4.position = [13, 0, 13]
 
 
-    # Cargamos la nave usando un modelo de la comunidad de Sketchfab.
-    nae = models_from_file("objects/space_shuttle.obj", "ship", 5, pipeline)[0] # Similar al resto de objetos, solo que ahora se usa "Ship" y se le otorga una velocidad diferente de 0.
-    nae.color = real_rgb(150, 140, 150)
-    nae.scale = [.5] * 3
-    nae.position = [2, 1, 0]
-
-
+    # Extra
     # Bonus: luna para un planeta, en este, este caso, del planeta 2. Se usa el objeto entregado por el cuerpo docente
     planet_2_moon = models_from_file("objects/planet.obj", "model", 0, pipeline)[0]
     planet_2_moon.color = real_rgb(70, 150, 80)
@@ -294,11 +307,11 @@ void main() {
 
 
     # En una lista metemos todos los objetos que se usaran en la escena.
-    scene = [sol, planet_1, planet_2, planet_3, planet_4, nae, planet_2_moon]
+    scene = [sol, planet_1, planet_2, planet_3, planet_4, planet_2_moon]
 
 
     # Cargamos la camara, recibe como parametro la nave/objeto que quiere seguir.
-    cam = Camara(nae)
+    cam = Camara(Spacecraft)
 
 
     # Dibjuamos en la ventana
@@ -307,18 +320,22 @@ void main() {
 
         glClearColor(0.1, 0.1, 0.1, 0.0)
 
+        glEnable(GL_DEPTH_TEST)
+
         window.clear()
 
         pipeline.use()
 
+        # Camara
         pipeline["view"] = cam.view()
-        pipeline["projection"] = Mat4.perspective_projection(WIDTH/HEIGHT, 1, 20, window.fov)
+        pipeline["projection"] = Mat4.perspective_projection(WIDTH/HEIGHT, 0.1, 10, window.fov)
 
-        pipeline["color"] = nae.color
-        pipeline["model"] = nae.model()
-        nae.draw()
+        # Nave
+        pipeline["color"] = Spacecraft.color
+        pipeline["model"] = Spacecraft.model()
+        Spacecraft.draw()
 
-        cam.target = nae
+        # Objetos de la escena
         for m in scene:
             pipeline["color"] = m.color
             pipeline["model"] = m.model()
@@ -344,10 +361,12 @@ void main() {
         planet_2_moon.position = [8*np.cos(0.1*dtheta)-0.5*np.cos(0.5*dtheta), 0, 4*np.sin(0.1*dtheta)-0.5*np.sin(0.5*dtheta)]
         planet_2_moon.rotation = [0, 1.2*dtheta, 0]
 
-        #nae.rotation = [np.cos(nae.pitch), np.sin(nae.yaw), 0]
+        Spacecraft.rotation = [0, -np.pi/2-Spacecraft.yaw, 0]
 
 
-        nae.update(dt)
+
+
+        Spacecraft.update(dt)
         cam.update(dt)
         window.time += dt
 
@@ -356,25 +375,25 @@ void main() {
     # Movimiento del mouse
     @window.event
     def on_mouse_motion(x, y, dx, dy):
-        nae.yaw += dx * nae.sensitivity
-        nae.pitch += dy * nae.sensitivity
-        nae.pitch = clamp(nae.pitch, -(np.pi /2), np.pi / 2)
+        Spacecraft.yaw += dx * Spacecraft.sensitivity
+        Spacecraft.pitch += dy * Spacecraft.sensitivity
+        Spacecraft.pitch = clamp(Spacecraft.pitch, -(np.pi /2), np.pi / 2)
 
 
     # Presionar tecla W o S
     @window.event
     def on_key_press(symbol, modifiers):
         if symbol == key.W:
-            nae.direction = 1
+            Spacecraft.direction = 1
         if symbol == key.S:
-            nae.direction = -1
+            Spacecraft.direction = -1
 
 
     # Soltar tecla W o S
     @window.event
     def on_key_release(symbol, modifiers):
         if symbol == key.W or symbol == key.S:
-            nae.direction = 0
+            Spacecraft.direction = 0
 
 
     pyglet.clock.schedule_interval(update, 1 / 60)
