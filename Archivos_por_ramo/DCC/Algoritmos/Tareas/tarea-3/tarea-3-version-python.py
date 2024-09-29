4b4e4340d7b450e2cda91bd7ab0796d33227c416
jorge <jorge.cumminz@gmail.com>
2024-09-29 02:07:37 -0300

    Tarea 3 de algoritmos con la parte izquierda que deberia printear lista

--- Archivos_por_ramo/DCC/Algoritmos/Tareas/tarea-3/tarea-3-version-python.py
+++ Archivos_por_ramo/DCC/Algoritmos/Tareas/tarea-3/tarea-3-version-python.py
@@ -3,6 +3,82 @@ Este codigo presenta el desarrollo de la tarea 3 en python en vez de un notebook
 comodamente.
 """
 
+# Esta función recibe un comando en string y el diccionario de variables. Con él,
+# procesa el comando, imprime el resultado de la expresion
+# y posiblemente modifica el diccionario. La función retorna el diccionario
+
+def procesar_comando(comando, dicc_var):
+    # procesa el comando e imprime el resultado
+    print(comando)
+    val_izquierdo = []
+    val_derecho = []
+    valores = []
+    calc = []
+    aux = []
+    parentesis_counter = 0
+    k = 0
+    activador = 0
+    while k<len(comando):
+        # Caso donde en vez de definir una variable se agrega un "="
+        if k == 0 and comando[k] == "=":
+            resultado = f'Error: al procesar "{comando[k]}"'
+            print(resultado)
+            break
+
+        # Caso donde al definir una variable se tiene un espacio en vez de un "_"
+        if comando[k] == " ":
+            resultado = f'Error: al procesar "{comando[k]}"'
+            print(resultado)
+            break
+
+        # Caso donde definimos una variable
+        if comando[k] == "=":
+            # Si despues del "=" no hay nada/no hay espacio o solamente operadores
+            if (k==len(comando)-1 or (k==len(comando)-2 and(comando[k+1]==" " or comando[k+1]=="+" or comando[k+1]=="-"
+                or comando[k+1]=="*" or comando[k+1]=="/" or comando[k+1]=="^"))):
+                resultado = f'Error: al procesar "{comando[k]+comando[k+1]}"'
+                print(resultado)
+                break
+            # Caso donde hay algo despues del "=" y tenemos que pasar al lado derecho
+            else:
+                val_izquierdo += [val_izquierdo[len(val_izquierdo)] + comando[k]]
+                valores += [val_izquierdo[len(val_izquierdo)]]
+                break
+
+        val_izquierdo += [val_izquierdo[len(val_izquierdo)]+comando[k]]
+        # Caso donde el ingresado corresponde a un valor pre ingresado y queremos ver cual es
+        if k == len(comando)-1:
+            # Si el valor pre ingresado no existe se lanza error
+            if not dicc_var[val_izquierdo[len(val_izquierdo)]]:
+                resultado = f'Error: variable indefinida "{val_izquierdo[len(val_izquierdo)]}"'
+                print(resultado)
+                break
+        # Si tenemos un valor pre ingresado, se printea
+        else:
+            resultado = f'{val_izquierdo[len(val_izquierdo)]} = {dicc_var[val_izquierdo[len(val_izquierdo)]]}'
+            print(resultado)
+            break
+        k += 1
+    return dicc_var
 
 
 
+
+
+
+def calculadora(lista_comandos):
+  # Este diccionario almacena las variables que se vayan definiendo en la calculadora
+  # Este diccionario se inicializa cuando se usa la calculadora
+  vars = dict()
+
+  # Se procesan todos los comandos de la lista (lista de string)
+  for i in range(0, len(lista_comandos)):
+    vars = procesar_comando(lista_comandos[i], vars)
+
+
+'''
+# EJEMPLO 1:
+lista = ["n=5","hanoi=2^n-1","var_1 = 23 - 13 + hanoi * 2","h2 = hanoi /2","","n"]
+calculadora(lista)
+
+'''
---
+++ Archivos_por_ramo/DCC/Algoritmos/Tareas/tarea-3/test.py
@@ -0,0 +1,10 @@
+dicc1 = {"opcion1": 1, "opcion2": 2}
+dicc2 = {"opcion3": 3, "opcion4": 4}
+# Hacer un diccionario que contenga a dicc1 y dicc2
+dicc1 = {**dicc1, **dicc2}
+print(dicc1)
+aaa = '123456'
+print(aaa[1])
+print(type(int(aaa[1])))
+print(dicc1["opcion5"])
+
