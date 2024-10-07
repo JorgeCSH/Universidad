3470a3c66f99020c7bea4e3e9fec4a603aec3429
JorgeCSH <jorge.cumminz@gmail.com>
2024-10-07 12:20:29 -0300

    Incluyo despues de bastantes peleas, la funcion en el archivo de la tarea

--- Archivos_por_ramo/DCC/Grafica/Tareas/Tarea_3/Tarea_3_Jorge_Cummins.py
+++ Archivos_por_ramo/DCC/Grafica/Tareas/Tarea_3/Tarea_3_Jorge_Cummins.py
@@ -103,6 +103,50 @@ def generate_ring(definition):
     return Model(positions, uv, None, indices)
 
 
+''' Funcion create_sphere()
+Funcion que, inspirada en la funcion create_ring, crea una
+esfera discretizando en coordenadas esfericas phi y theta.
+
+Recibe "definition" que corresponde a la cantidad de divisiones y devuelve Model para crear la esfera.
+'''
+def create_sphere(definition):
+    # coordenadas de posición
+    positions = np.zeros((definition)*3*3 , dtype=np.float32) 
+    # coordenadas de texturas
+    uv = np.zeros((definition)*3*3, dtype=np.float32)
+    dtheta = 2*np.pi / definition
+    dphi = np.pi / definition
+    r = 1.0
+
+    for i in range(definition):
+        idx = 3*i
+        tidx = 2*i
+        phi = (i+1)*dphi
+        theta = i*dtheta
+        positions[idx:idx+3] = [np.cos(theta)*np.sin(phi)*r, np.sin(theta)*np.sin(phi)*r, np.cos(phi)*r] 
+        uv[tidx:tidx+2] = [theta/(2*np.pi), phi/np.pi]
+        if i%2==0:
+            uv[tidx:tidx+2] = [0, 1]
+        else:
+            uv[tidx:tidx+2] = [0, 0]
+    
+    indices = np.zeros(6*definition, dtype=np.int32)
+    for i in range(definition-1):
+        idx = 6*i
+        # t0
+        indices[idx:idx+3] = [i, i+1, i+definition]
+        # t1
+        indices[idx+3:idx+6] = [i+1, i+definition+1, i+definition]
+   
+    # Completamos la esfera
+    idx = 6*(definition-1)
+    # t0
+    indices[idx:idx+3] = [definition-1, 0, definition-1+definition]
+    # t1
+    indices[idx+3:idx+6] = [0, definition, definition-1+definition]
+    return Model(positions, uv, None, indices)
+
+
 # Seccion 4: Configuracion de la escena ###############################################################################
 ###########################################################################################################################
 '''
