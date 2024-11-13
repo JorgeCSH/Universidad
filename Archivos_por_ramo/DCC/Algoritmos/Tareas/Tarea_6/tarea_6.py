d9fcae1a0d57e31865402e196117e1b4430c410a
JorgeCSH <jorge.cumminz@gmail.com>
2024-11-13 00:20:56 -0300

    Lee la descripcion... Buckle your seat belts boys, girls or whoever is reading this at this spectacular time in your life but my Professor (your day might as well be ruined by this monstrosity) because in just three days I’ll be starting (and hopefully finishing) a full-blown mf algorithms and data structures homework assignment. Yes, the very same assignment I should have started over a week ago, from a subject I only managed to cram into my brain two hours ago after missing more than three consecutive weeks of lectures. So grab a chair, mute all distractions, and prepare yourself for the 3-AM-Monster-Energy-ADHD-meds-fueled-self-hatred-driven extravaganza of code you didn’t know you needed, all of this focused on the magnificent 2-3 tree data structure. What you’re about to read is damn the best thing you will probably (hopefully not) experience within a 30-minute window.

--- Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/tarea_6.py
+++ Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/tarea_6.py
@@ -38,74 +38,104 @@ La operación de búsqueda (``search``) debe retornar un puntero al nodo que con
 exitosa, o ``None`` si es infructuosa
 '''
 
-'''
-def search(self,x):
-    return None # Aquí hay que retornar el resultado de buscar x a partir de este punto
-                # Esto puede ser un puntero al nodo en donde está x, o None si no está
-'''
-'''
-def search(self,x):
-    return None # Aquí hay que retornar el resultado de buscar x a partir de este punto
-                # Esto puede ser un puntero al nodo en donde está, o None si no está
-'''
-
-
 class Nodo2:
     def __init__(self, izq, info, der):
-        self.izq=izq
-        self.info=info
-        self.der=der
+        self.izq = izq
+        self.info = info
+        self.der = der
 
-    def insert(self,x):
-        return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+    def insert(self, x):
+        # Caso donde se inserta en el arbol de la izquierda
+        if x < self.info:
+            arbol_izquierdo_viejo = self.izq.insert(x)
 
-    def search(self,x):
-      # Caso donde vemos si el valor buscado es menor al del nodo.
-      if x < self.info:
-        return self.izq.search(x)
+            # Caso donde no ocurre un split, no deberia ser una tupla y aprovechamos para diferenciar casos.
+            if not type(arbol_izquierdo_viejo) == tuple:
+                self.izq = arbol_izquierdo_viejo
 
-      # Caso donde vemos si el valor buscado es mayor al del nodo.
-      elif x > self.info:
-        return self.der.search(x)
+            # Caso donde ocurre un split.
+            else:
+                # Aca son todos los casos donde no son no tupla, por ende sea agrega una prueba (assert).
+                assert type(arbol_izquierdo_viejo) == tuple
+                izq_nuevo, med, der_nuevo = arbol_izquierdo_viejo
+                return Nodo3(izq_nuevo, med, der_nuevo, self.info, self.der)
 
-      # Caso sobrante es y sin error es que sea igual.
-      return self
+        # Caso donde se inserta en el arbol de la derecha
+        else:
+            # Si bien puede ser reduntante, es para asegurar que no se agrega el mismo.
+            assert x > self.info
+            arbol_derecho_viejo = self.der.insert(x)
 
+            # Caso donde no ocurre un split, analogo al caso del lado izquierdo.
+            if not type(arbol_derecho_viejo) == tuple:
+                self.der = arbol_derecho_viejo
+
+            # Caso donde ocurre un split, analogo al caso anterior.
+            else:
+                assert type(arbol_derecho_viejo) == tuple
+                izq_nuevo, med, der_nuevo = arbol_derecho_viejo
+                return Nodo3(self.izq, self.info, izq_nuevo, med, der_nuevo)
+            return self
+
+    def search(self, x):
+        if x == self.info:
+            return self
+        elif x < self.info:
+            return self.izq.search(x)
+        elif x > self.info:
+            return self.der.search(x)
+        else:
+            return None
 
     def string(self):
         return ("("+self.izq.string()
                 +str(self.info)
                 +self.der.string()+")")
 
+
 class Nodo3:
     def __init__(self, izq, info1, med, info2, der):
-        self.izq=izq
-        self.info1=info1
-        self.med=med
-        self.info2=info2
-        self.der=der
+        self.izq = izq
+        self.info1 = info1
+        self.med = med
+        self.info2 = info2
+        self.der = der
 
-    def insert(self,x):
-        return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+    def insert(self, x):
+        if x < self.info1:
+            resultado = self.izq.insert(x)
+            if isinstance(resultado, tuple):  # Se produjo una división en el hijo izquierdo
+                izq_nuevo, med, der_nuevo = resultado
+                return Nodo3(izq_nuevo, med, der_nuevo, self.info1, self.med), self.info2, self.der
+            else:
+                self.izq = resultado
+        elif x < self.info2:
+            resultado = self.med.insert(x)
+            if isinstance(resultado, tuple):  # Se produjo una división en el hijo medio
+                izq_nuevo, med, der_nuevo = resultado
+                return Nodo3(self.izq, self.info1, izq_nuevo, med, der_nuevo), self.info2, self.der
+            else:
+                self.med = resultado
+        else:
+            resultado = self.der.insert(x)
+            if isinstance(resultado, tuple):  # Se produjo una división en el hijo derecho
+                izq_nuevo, med, der_nuevo = resultado
+                return Nodo3(self.izq, self.info1, self.med, self.info2, izq_nuevo), med, der_nuevo
+            else:
+                self.der = resultado
+        return self
 
-    def search(self,x):
-        # Caso donde x es igual a la llave/info1.
-        if x == self.info1:
-          return self
-
-        # Caso donde x es igual a la llave/info2.
-        elif x == self.info2:
-          return self
-
-        # Caso donde es menor que info1, se baja a la izquierda.
+    def search(self, x):
+        if x == self.info1 or x == self.info2:
+            return self
         elif x < self.info1:
             return self.izq.search(x)
-
-        # Caso donde es mayot que info2, se baja a la derecha.
+        elif x < self.info2:
+            return self.med.search(x)
         elif x > self.info2:
             return self.der.search(x)
-
-        return self.med.search(x)
+        else:
+            return None
 
     def string(self):
         return ("("+self.izq.string()
@@ -114,49 +144,75 @@ class Nodo3:
                 +str(self.info2)
                 +self.der.string()+")")
 
+
 class Nodoe:
     def __init__(self):
         pass
 
-    def insert(self,x):
-        return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+    def insert(self, x):
+
+        return Nodoe(), x, Nodoe()
 
     def string(self):
         return"☐"
 
-    # Metodo provisorio, probablemente reemplazar en el arbol
-    def search(self,x):
-        return None
-
 
 class Arbol23:
-    def __init__(self,raiz=Nodoe()):
-        self.raiz=raiz
+    def __init__(self, raiz=Nodoe()):
+        self.raiz = raiz
 
-    def insert(self,x):
-        self.raiz=self.raiz.insert(x)   # Esto funciona casi siempre, excepto cuando la antigua raíz se divide
-                                        # Usted tiene que poner algo que funcione siempre
+    def insert(self, x):
+        inserta_raiz = self.raiz.insert(x)
+        if not type(inserta_raiz) == tuple:
+            self.raiz = inserta_raiz
+        else:                                               # La raíz se dividió
+            assert type(inserta_raiz) == tuple
+            izq_nuevo, med, der_nuevo = inserta_raiz
+            self.raiz = Nodo2(izq_nuevo, med, der_nuevo)
 
-    def search(self,x):
+    def search(self, x):
         return self.raiz.search(x)
 
     def imprimir(self):
         print(self.raiz.string())
+        return self.raiz.string()
 
 
+# Tests
+# Tests originales
 # Para probar este código, vamos a construir "a mano" el árbol 2-3 que aparece en el apunte, y luego imprimirlo
 a=Nodo3(Nodo2(Nodoe(),10,Nodoe()),25,Nodo3(Nodoe(),32,Nodoe(),48,Nodoe()),57,Nodo2(Nodoe(),74,Nodoe()))
-print(a.string())
+#print(a.string())
 
-#print(a.search(25).string())
+# Test solicitados e implementados por yo
+
+gogos = Arbol23()
+gogos.insert(3)
+gogos.insert(1)
+gogos.insert(4)
+gogos.insert(5)
+gogos.insert(9)
+gogos.insert(6)
+gogos.insert(2)
+gogos.imprimir()
+#assert gogos.imprimir() == "((☐1☐2☐)3(☐4☐)5(☐6☐9☐))"
+#gogos.imprimir()
+
+#assert gogos.imprimir()  == "(((☐2☐3☐)4☐5☐)6☐9☐)"
+
+print()
+terrenator = Arbol23()
+terrenator.insert(10)
+terrenator.insert(25)
+terrenator.insert(32)
+terrenator.insert(48)
+terrenator.insert(57)
+terrenator.insert(74)
+#print(a.string())
+#terrenator.imprimir()
 
 
 #((☐10☐)25(☐32☐48☐)57(☐74☐))
 
-'''
-Corregir o estar atento
-- Por ahora estoy haciendo uso de un metodo que  me saque de la raja, atento.
-- Por ahora search es literal lo que  implemente en mi ejercicio 9.
 
-'''
 
