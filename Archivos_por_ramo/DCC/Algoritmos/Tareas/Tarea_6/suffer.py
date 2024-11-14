c2089a3afce82b0fec4da9543985b65bda533d1f
JorgeCSH <jorge.cumminz@gmail.com>
2024-11-13 23:59:52 -0300

    Ya si no queria ser ingeniero

--- Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/suffer.py
+++ Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/suffer.py
@@ -7,37 +7,27 @@ class Nodo2:
         self.der = der
 
     def insert(self, x):
-        # Caso donde se inserta en el arbol de la izquierda
         if x < self.info:
+            # La insertamos recursivamente en el hijo izquierdo
             arbol_izquierdo_viejo = self.izq.insert(x)
+            
+            # Caso donde se produce split en el nodo hijo.
+            if isinstance(arbol_izquierdo_viejo, tuple):
+                izq_nuevo, llave, der_nuevo = arbol_izquierdo_viejo
+                return Nodo3(izq_nuevo, llave, self.izq, self.info, self.der)
+            # Si no se produjo split 
+            return self.izq.insert(x)
 
-            # Caso donde no ocurre un split, no deberia ser una tupla y aprovechamos para diferenciar casos.
-            if not isinstance(arbol_izquierdo_viejo, tuple):
-                return Nodo2(arbol_izquierdo_viejo, x, self.der) 
-            # Caso donde ocurre un split.
-            else:
-                # Aca son todos los casos donde no son no tupla.
-                izq_nuevo, med, der_nuevo = arbol_izquierdo_viejo
-                return Nodo3(izq_nuevo, med, der_nuevo, self.info, self.der)
-
-        # Caso donde se inserta en el arbol de la derecha
         else:
-            # Si bien puede ser reduntante, es para asegurar que no se agrega el mismo.
-            assert x > self.info
+            # La insertamos recursivamente en el hijo derecho 
             arbol_derecho_viejo = self.der.insert(x)
 
-            # Caso donde no ocurre un split, analogo al caso del lado izquierdo.
-            if not type(arbol_derecho_viejo) == tuple:
-                self.der = arbol_derecho_viejo
-                self.der
-
-            # Caso donde ocurre un split, analogo al caso anterior.
-            else:
-                assert type(arbol_derecho_viejo) == tuple
+            if isinstance(arbol_derecho_viejo, tuple):
                 izq_nuevo, med, der_nuevo = arbol_derecho_viejo
                 return Nodo3(self.izq, self.info, izq_nuevo, med, der_nuevo)
-            return self
-    
+            
+            return self.der.insert(x)
+        return self 
     # Adaptacoion de lo realizado en el ejercicio 8 
     def search(self, x):
         # Caso donde debemos buscar por el arbol izquierdo
@@ -69,36 +59,32 @@ class Nodo3:
 
     def insert(self, x):
         if x < self.info1:
-            resultado = self.izq.insert(x)
+            arbol_izquierdo_viejo = self.izq.insert(x)
             
-            if isinstance(resultado, tuple):  # Se produjo una división en el hijo izquierdo
-                izq_nuevo, med, der_nuevo = resultado
+            if isinstance(arbol_izquierdo_viejo, tuple):  # Se produjo una división en el hijo izquierdo
+                izq_nuevo, med, der_nuevo = arbol_izquierdo_viejo
                 return Nodo3(izq_nuevo, med, der_nuevo, self.info1, self.med), self.info2, self.der
             
-            else:
-                self.izq = resultado
+            return arbol_izquierdo_viejo
 
         elif x < self.info2:
-            resultado = self.med.insert(x)
+            arbol_medio_viejo = self.med.insert(x)
 
-            if isinstance(resultado, tuple):  # Se produjo una división en el hijo medio
-                izq_nuevo, med, der_nuevo = resultado
+            if isinstance(arbol_medio_viejo, tuple):  # Se produjo una división en el hijo medio
+                izq_nuevo, med, der_nuevo = arbol_medio_viejo
                 return Nodo3(self.izq, self.info1, izq_nuevo, med, der_nuevo), self.info2, self.der
 
-            else:
-                self.med = resultado
+            return arbol_medio_viejo
 
         else:
-            resultado = self.der.insert(x)
+            arbol_derecho_viejo = self.der.insert(x)
 
-            if isinstance(resultado, tuple):  # Se produjo una división en el hijo derecho
-                izq_nuevo, med, der_nuevo = resultado
+            if isinstance(arbol_derecho_viejo, tuple):  # Se produjo una división en el hijo derecho
+                izq_nuevo, med, der_nuevo = arbol_derecho_viejo
                 return Nodo3(self.izq, self.info1, self.med, self.info2, izq_nuevo), med, der_nuevo
 
-            else:
-                self.der = resultado
+            return arbol_derecho_viejo
 
-        return self
 
     # Adaptado de lo realizado en el ejercicio 8, analogo al caso de Nodo2 en terminos de cambios.
     def search(self, x):
@@ -147,12 +133,11 @@ class Arbol23:
 
     def insert(self, x):
         inserta_raiz = self.raiz.insert(x)
-        if not type(inserta_raiz) == tuple:
-            self.raiz = inserta_raiz
-        else:                                               # La raíz se dividió
-            assert type(inserta_raiz) == tuple
+        if isinstance(inserta_raiz, tuple):
             izq_nuevo, med, der_nuevo = inserta_raiz
             self.raiz = Nodo2(izq_nuevo, med, der_nuevo)
+        else:                                               # La raíz se dividió
+            self.raiz = inserta_raiz
 
     def search(self, x):
         return self.raiz.search(x)
@@ -171,23 +156,20 @@ a=Nodo3(Nodo2(Nodoe(),10,Nodoe()),25,Nodo3(Nodoe(),32,Nodoe(),48,Nodoe()),57,Nod
 # Test solicitados e implementados por yo
 
 gogos = Arbol23()
-gogos.insert(3)
-gogos.insert(1)
-gogos.insert(4)
-gogos.insert(5)
-gogos.insert(9)
-gogos.insert(6)
-gogos.insert(2)
+chococrispis = [3, 1, 4, 5, 9, 6, 2]
+for i in chococrispis:
+    gogos.insert(i)
+    gogos.imprimir()
 gogos.imprimir()
-gogos.search(2)
-gogos.search(3)
+#gogos.search(2)
+#gogos.search(3)
 #gogos.search(7)
 
 #assert gogos.imprimir() == "((☐1☐2☐)3(☐4☐)5(☐6☐9☐))"
 #gogos.imprimir()
 
 #assert gogos.imprimir()  == "(((☐2☐3☐)4☐5☐)6☐9☐)"
-
+'''
 print()
 terrenator = Arbol23()
 terrenator.insert(10)
@@ -201,6 +183,6 @@ terrenator.insert(74)
 
 
 #((☐10☐)25(☐32☐48☐)57(☐74☐))
-
+'''
 
 
--- Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/tarea_6.py
+++ Archivos_por_ramo/DCC/Algoritmos/Tareas/Tarea_6/tarea_6.py
@@ -39,6 +39,11 @@ indicaría que la raíz se dividió. En ese caso, hay que crear nodo binario par
 La operación de búsqueda (``search``) debe retornar un puntero al nodo que contiene la llave buscada, en caso de ser
 exitosa, o ``None`` si es infructuosa
 '''
+############################################################################################################
+
+""" Clase Nodo2.
+Representa a los nodos binarios presentes en el arbol.
+"""
 class Nodo2:
     def __init__(self, izq, info, der):
         self.izq=izq
@@ -46,22 +51,34 @@ class Nodo2:
         self.der=der
 
     def insert(self,x):
-        return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+        #return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+        # Caso donde debemos insertar en el hijo izquierdo
+        if x<self.info:
+            # Caso donde no se produce split en el hijo entregamos puntero al nodo resultante
+            if not isinstance(self.izq.insert(x), Nodo3):
+                self.izq = self.izq.insert(x)
+                return self.izq 
+            # Caso donde se produce split en el hijo
+            else:
+                izq_nuevo, llave, der_nuevo = self.izq.insert(x)
+                return (izq_nuevo, llave, Nodo2(der_nuevo, self.info, self.der))
 
     # Adaptacion de lo realizado en el ejercicio 8 
     def search(self, x):
+        # Retornamos None si el siguiente es Nodoe
+        if isinstance(self, Nodoe):
+            return None
+        # Caso donde encontramos el valor
+        elif x == self.info:
+            return self
         # Caso donde debemos buscar por el arbol izquierdo
-        if x < self.info:
+        elif x < self.info:
             return self.izq.search(x)
         # Caso donde debemos buscar por el arbol derecho
         elif x > self.info:
             return self.der.search(x)
-        # Caso donde encontramos el valor
-        elif x == self.info:
-            return self
-        # Este es el cambio, para tener incluido el caso en que el valor no este. (CORREGIR)
-        else:
-            return None
+
+
 
     def string(self):
         return ("("+self.izq.string()
@@ -69,6 +86,10 @@ class Nodo2:
                 +self.der.string()+")")
 
 
+
+""" Clase Nodo3.
+Clase/estructura que representa los nodos ternarios en el arbol.
+"""
 class Nodo3:
     def __init__(self, izq, info1, med, info2, der):
         self.izq=izq
@@ -80,10 +101,15 @@ class Nodo3:
     def insert(self,x):
         return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
 
+
+
     # Adaptado de lo realizado en el ejercicio 8, analogo al caso de Nodo2 en terminos de cambios.
     def search(self, x):
+        # Retornamos None si el siguiente es Nodoe
+        if isinstance(self, Nodoe):
+            return None
         # Caso donde encontramos alguno de los valores.
-        if x == self.info1 or x == self.info2:
+        elif x == self.info1 or x == self.info2:
             return self
         # Caso donde debemos buscar por el arbol izquierdo.
         elif x < self.info1:
@@ -95,9 +121,7 @@ class Nodo3:
         # Caso donde debemos buscar por el arbol derecho.
         elif x > self.info2:
             return self.der.search(x)
-        # Caso nuevo, no encontramos el valor. (CORREGIR)
-        else:
-            return None
+
 
     def string(self):
         return ("("+self.izq.string()
@@ -107,23 +131,27 @@ class Nodo3:
                 +self.der.string()+")")
 
 
+""" Clase Nodoe.
+Clase que representa a los nodos externos/hojas del arbol. Eta es representada por un cuadradito.
+"""
 class Nodoe:
     def __init__(self):
         pass
 
-    #def insert(self,x):
-    #    return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
+    def insert(self,x):
+        return None # Aquí hay que retornar el resultado de insertar x a partir de este punto
     
-    # Metodo provisorio
-    def insert(self, x):
-        return Nodoe(), x, Nodoe()
-    
-    # Metodo provisorio.
+    # Metodo provisorio... E L I M I N A R def search(self, x):
+        return None
 
     def string(self):
         return"☐"
 
 
+
+""" Clase Arbol23
+Clase encargada de formar la estructura del arbol 2-3, esta hace uso de las clases Nodo2 y Nodo3.
+"""
 class Arbol23:
     def __init__(self,raiz=Nodoe()):
         self.raiz=raiz
@@ -132,25 +160,40 @@ class Arbol23:
         self.raiz=self.raiz.insert(x)   # Esto funciona casi siempre, excepto cuando la antigua raíz se divide
                                         # Usted tiene que poner algo que funcione siempre
     def search(self,x):
-        return self.raiz.search(x)
+        if isinstance(self.raiz, Nodoe):
+            return None
+        else:
+            return self.raiz.search(x)
 
     def imprimir(self):
         print(self.raiz.string())
 
+
+
+"""
+Test playground
+"""
 # Ejemplo original sin el uso del arbol
 a=Nodo3(Nodo2(Nodoe(),10,Nodoe()),25,Nodo3(Nodoe(),32,Nodoe(),48,Nodoe()),57,Nodo2(Nodoe(),74,Nodoe()))
-print(a.string())
+#print(a.search(10))
+#print(a.search(25))
+#print(a.search(57))
+#print(a.search(69))
 
-# Aca usaremos el arbol.
+# Ejemplo donde nosotros implementaremos un arbol.
+# Formamos el arbol
 santa_claus_llego_a_la_ciudad = Arbol23()
-santa_claus_llego_a_la_ciudad.insert(3)
-santa_claus_llego_a_la_ciudad.insert(1)
-santa_claus_llego_a_la_ciudad.insert(4)
-santa_claus_llego_a_la_ciudad.insert(5)
-santa_claus_llego_a_la_ciudad.insert(9)
-santa_claus_llego_a_la_ciudad.insert(6)
-santa_claus_llego_a_la_ciudad.insert(2)
-santa_claus_llego_a_la_ciudad.imprimir()
+# Insertamos los valores
+"""
+valores_a_insertar = [3, 1, 4, 5, 9, 6, 2]
+for i in valores_a_insertar:
+    santa_claus_llego_a_la_ciudad.insert(i)
+    santa_claus_llego_a_la_ciudad.imprimir()
+"""
+# Imprimimos el arbol final (ademas de donde imprimiamos cada iteracion
+#santa_claus_llego_a_la_ciudad.imprimir()
+
+# Realizamos las busquedas                             
 santa_claus_llego_a_la_ciudad.search(2)
 santa_claus_llego_a_la_ciudad.search(3)
 #santa_claus_llego_a_la_ciudad.search(7)
